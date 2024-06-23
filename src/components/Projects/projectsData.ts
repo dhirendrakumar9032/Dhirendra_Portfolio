933a7cd8feec76830341ea63a55c87ccbe2705ca
dhirendra.kumar <dhirendra.kumar@qapita.com>
2024-06-23 22:41:52 +0530

    added new project

--- src/components/Projects/projectsData.ts
+++ src/components/Projects/projectsData.ts
@@ -10,6 +10,27 @@ export interface ProjectType {
 
 export const projects: ProjectType[] = [
   {
+    title: "Admin UI GeekTrust Challenge",
+    description:
+      "The Admin UI Geektrust Project is a user management system that provides functionalities for searching, editing, deleting, and paginating user records. The project leverages various modern web development technologies to deliver a responsive and user-friendly interface.",
+    technology: [
+      "React",
+      "TypeScript",
+      "SCSS",
+      "Zustand",
+      "React Quary"
+     
+    ],
+    id: 1,
+    projectImgLinks: [
+      "https://i.imgur.com/dbv6Ifw.png",
+      "https://imgur.com/dbv6Ifw.png",
+      "https://imgur.com/i6MIojM.png"
+    ],
+    liveDemoUrl:'https://admin-ui-geektrust-project-june-2024.vercel.app/',
+    githubUrl: "https://github.com/dhirendrakumar9032/admin-ui-geektrust-project-june-2024.git",
+  },
+  {
     title: "Portfolio",
     description:
       "An interactive portfolio showcasing my skills and projects as a Frontend Engineer. The site features a modern and clean design with engaging animations, a professional overview, a detailed project section with Swiper for carousel functionality, and a dynamic contact form. Key sections highlight my technical skills, work experience, and a personal introduction.",
@@ -23,7 +44,7 @@ export const projects: ProjectType[] = [
       "Swiper",
       "Ant Design",
     ],
-    id: 3,
+    id: 2,
     projectImgLinks: [
       "https://i.imgur.com/qMP0ckA.png",
       "https://i.imgur.com/FH62HtN.png",
@@ -36,7 +57,7 @@ export const projects: ProjectType[] = [
     description:
       `This project is a Kanban dashboard created with Next.js and TypeScript, styled using Tailwind CSS. It features a dark-themed UI with columns for tasks categorized as "Todo," "Work in progress," and "Done," each column containing task cards. Users can add tasks to each column and potentially move them between columns to track the progress of their work. `,
     technology: ["Next js", "TypeScript", "Tailwind", "SCSS"],
-    id: 4,
+    id: 3,
     projectImgLinks: [
       "https://i.imgur.com/kUTxxCv.png",
       "https://i.imgur.com/5dfifg8.png",
